34d1cf5e661f9682c9aea9409953e2b780716f7b
Akash Basu <abasu924@gmail.com>
2025-09-19 18:38:32 -0400

    Update part1_plot.py

--- part1_plot.py
+++ part1_plot.py
@@ -6,4 +6,12 @@ train  = pd.read_csv("train.csv")
 
 
 #cont feature
-cf = 
+cf = "GrLivArea"
+
+
+plt.figure(figsize=(8, 5))
+sns.histplot(train[cf], bins=40, kde=True)
+plt.title(f"GRLivArea")
+plt.xlabel(cf)
+plt.ylabel("Count")
+plt.show()
