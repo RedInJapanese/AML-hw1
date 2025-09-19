d491285dfeab86d636338e1e57e7d5d4539bf708
Akash Basu <abasu924@gmail.com>
2025-09-19 18:48:45 -0400

    Update part1_plot.py

--- part1_plot.py
+++ part1_plot.py
@@ -15,3 +15,18 @@ plt.title(f"GRLivArea")
 plt.xlabel(cf)
 plt.ylabel("Count")
 plt.show()
+
+#categorical feature
+cf2 = "Neighborhood"  
+
+plt.figure(figsize=(12, 6))
+sns.countplot(y=cf2, data=train, order=train[cf2].value_counts().index)
+plt.title(f"Distribution of {cf2}")
+plt.xlabel("Count")
+plt.ylabel(cf2)
+plt.show()
+
+
+
+
+#part 1 question 4
